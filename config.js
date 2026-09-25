4e80e26d7f78e01b07c2e285713cb7fdff730f86
rafis-1976 <webkaa@yahoo.es>
2026-09-25 06:47:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 
 window.MIGARAJE_CONFIG = {
   SUPABASE_URL:      'https://mwzhyozqmsqmfpgtzeek.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_C8wBfWO_rnKjffPtc4DOQA_tkVY7xVo'
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im13emh5b3pxbXNxbWZwZ3R6ZWVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyMjE3ODEsImV4cCI6MjEwNTc5Nzc4MX0.U03Ec0QqhWznmy9_pyyjvp0yS9vzuPy9FY01UvfZDs0'
 };
